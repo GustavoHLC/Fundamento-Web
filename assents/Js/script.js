68dbfeefcc60c48aeb504e70e8a6df9a6b7fccda
GustavoHLC <gutohenri_car@hotmail.com>
2021-07-20 17:01:22 -0300

    Implementação de JavaScript

--- assents/Js/script.js
+++ assents/Js/script.js
@@ -16,10 +16,10 @@ email.style.width = '100%'
 function validaNome() {
     let txtNome = document.querySelector('#txtNome')
     if(nome.value.length < 3){
-        txtNome.innerHTML = 'Nome Inválido'
+        txtNome.innerHTML = '❌'
         txtNome.style.color = 'red'
     } else {
-        txtNome.innerHTML = 'Nome Válido'
+        txtNome.innerHTML = '✔'
         txtNome.style.color = 'green'
     }
 }
@@ -27,10 +27,10 @@ function validaNome() {
 function validaEmail() {
     let txtEmail = document.querySelector('#txtEmail')
     if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1) {
-        txtEmail.innerHTML = 'E-mail inválido'
+        txtEmail.innerHTML = '❌'
         txtEmail.style.color = 'red'
     } else {
-        txtEmail.innerHTML = 'E-mail Válido'
+        txtEmail.innerHTML = '✔'
         txtEmail.style.color = 'green'
     }
 }
